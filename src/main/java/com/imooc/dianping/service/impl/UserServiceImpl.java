cd92253c52bfb3807e4fa774b11dda29145e6244
FigSprite <figsprite@gmail.com>
2020-03-21 16:54:10 +0800

    初始化

---
+++ src/main/java/com/imooc/dianping/service/impl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.imooc.dianping.service.impl;
+
+import com.imooc.dianping.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+}
